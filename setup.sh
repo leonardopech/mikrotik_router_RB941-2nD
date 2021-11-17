a2143666d3f4f180aac5759cfe67fa3bf13e59f9
leonardopech <52260498+leonardopech@users.noreply.github.com>
2021-11-16 23:14:41 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+/interface bridge
+add name=WAN
+add name=LAN_CLIENTES
+
+/interface bridge port
+add bridge=WAN interface=ether1
+add bridge=LAN_CLIENTES interface=ether2
+add bridge=LAN_CLIENTES interface=ether3
+add bridge=LAN_CLIENTES interface=ether4
+
+/ip address
+add address=192.168.39.254/24 interface=WAN
+add address=192.168.50.1/24 interface=LAN_CLIENTES
+
+/ip route
+add gateway=192.168.39.1
+
+/ip dns
+set servers=1.1.1.1,8.8.8.8
+
+/ip firewall nat
+add chain=srcnat action=masquerade out-interface=WAN
